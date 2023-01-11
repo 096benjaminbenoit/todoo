a14520a15fcd19f1a7fbc0bacda8b57336dde01c
Benjamin Benoit <119290452+096benjaminbenoit@users.noreply.github.com>
2023-01-11 11:45:21 +0100

    maj 11:45

--- script.js
+++ script.js
@@ -18,13 +18,28 @@ function addTask() {
     addDiv.append(addText);
     addText.append(inputElt.value);
 
+    // création du bouton de modification
+    let addButtonModified = document.createElement("button");
+    addDiv.append(addButtonModified);
+    addButtonModified.classList.add("container_list_task--modified");
+
     // création du bouton de suppression
     let addButtonDelete = document.createElement("button");
     addDiv.append(addButtonDelete);
     addButtonDelete.classList.add("container_list_task--delete");
-    }
-    // permet de reset l'input
+
+    // permet de reset l'input une fois la tache ajoutée
     formElt.reset();
+
+    const modifiedButton = document.querySelector('.container_list_task--modified');
+
+    modifiedButton.addEventListener('click', function modifiedTask() {
+        let addText = document.querySelector('.container_list_task--text');
+        let addDiv = document.querySelector(".container_list_task");
+        let newInput = document.createElement("input");
+        addDiv.replaceChildren(newInput, addDiv.childNodes[1]);
+        })
+    }
 }
 
 buttonElt.addEventListener('click', addTask);
