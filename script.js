3b069754247f1c3571c48300d819c664d28015c2
Benjamin Benoit <119290452+096benjaminbenoit@users.noreply.github.com>
2023-01-11 08:42:16 +0100

    maj js / css

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const formElt = document.querySelector(".container_add_form")
+const formElt = document.querySelector(".container_add_form");
 const buttonElt = document.querySelector("#add-button");
 const inputElt = document.querySelector("#add-text");
 const listElt = document.querySelector("#add-list");
 
 function addTask() {
-    if(inputElt.value.length <= 0) {
-        alert('bablabla')
+    if(inputElt.value.trim() == "") {
+        alert('Veuillez saisir une tâche');
     } else {
+    // création de la div
     let addDiv = document.createElement("div");
     listElt.append(addDiv);
     addDiv.classList.add("container_list_task");
 
+    // création du paragraphe avec la value de l'input
     let addText = document.createElement("p");
     addText.classList.add("container_list_task--text");
     addDiv.append(addText);
     addText.append(inputElt.value);
-    }
 
+    // création du bouton de suppression
+    let addButtonDelete = document.createElement("button");
+    addDiv.append(addButtonDelete);
+    addButtonDelete.classList.add("container_list_task--delete");
+    }
+    // permet de reset l'input
     formElt.reset();
 }
 
 buttonElt.addEventListener('click', addTask);
 
+// le formulaire ne s'envoie pas en appuyant sur entrée
 inputElt.addEventListener("keypress", function(pressEnter) {
     if (pressEnter.key === "Enter") {
         pressEnter.preventDefault();
